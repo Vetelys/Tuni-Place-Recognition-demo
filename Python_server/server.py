0313c1c5e88135a662ad03d1bfcb7c272208b6a4
joona <joonan97@gmail.com>
2021-10-22 21:39:09 +0300

    server has different routes for uploading of images and sending result of estimation

--- Python_server/server.py
+++ Python_server/server.py
@@ -7,8 +7,8 @@ import os
 app = flask.Flask(__name__)
 
 
-@app.route('/', methods=['GET', 'POST'])
-def handle_request():
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_image():
     try:
         imagefile = flask.request.files['image']
         filename = werkzeug.utils.secure_filename(imagefile.filename)
@@ -25,6 +25,20 @@ def handle_request():
         print(err)
         return "Error, request did not contain an image."
 
+@app.route('/result', methods=['GET', 'POST'])
+def set_result():
+    try:
+        print(flask.request.form)
+        img_filename = flask.request.form['image_name']
+        result = flask.request.form['result']
+        if result == "Correct":
+            os.rename(os.getcwd()+"\\"+img_filename, os.getcwd()+"\\Correct\\"+img_filename)
+        elif result == "False":
+            os.rename(os.getcwd()+"\\"+img_filename, os.getcwd()+"\\False\\"+img_filename)
+    except Exception as err:
+        print("Error: ")
+        print(err)
+        return "Error, request did not contain needed data"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
